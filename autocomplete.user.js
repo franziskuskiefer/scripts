1992801aa20654e7bd5a917a80fdc8cf21ed3837
Franziskus Kiefer <franziskuskiefer@gmail.com>
2014-02-02 15:23:03 +0000

    added surrey remote

--- autocomplete.user.js
+++ autocomplete.user.js
@@ -1,9 +1,23 @@
 // ==UserScript==
 // @name SurreyPasswords
 // @description enable auto complete at Surrey learn website for login form
-// @namespace https://surreylearn.surrey.ac.uk/
-// @include https://surreylearn.surrey.ac.uk/*
+// @namespace https://www.franziskuskiefer.de
+// @include *surreylearn.surrey.ac.uk/*
+// @include https://remote.surrey.ac.uk/*
 // @version 1
 // ==/UserScript==
-document.getElementById("Username").autocomplete = "on"
-document.getElementById("Password").autocomplete = "on"
+
+// redirect surrey learn to https
+if (document.URL == "http://surreylearn.surrey.ac.uk/")
+	window.location.replace("https://surreylearn.surrey.ac.uk");
+	
+// enable autocomplete at surrey learn
+if (document.URL == "https://surreylearn.surrey.ac.uk/") {
+	document.getElementById("Username").autocomplete = "on";
+	document.getElementById("Password").autocomplete = "on";
+}
+
+// enable autocomplete at remote surrey
+if (document.URL.indexOf("https://remote.surrey.ac.uk") == 0) {
+	document.getElementsByName("frmLogin")[0].autocomplete = "on";
+}
