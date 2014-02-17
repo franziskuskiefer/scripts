e39b4937a700e280bfc668e922bea80d0c9be78c
Franziskus Kiefer <franziskuskiefer@gmail.com>
2014-02-17 19:42:42 +0000

    added *.bcf to rmtexaux

--- rmtexaux.sh
+++ rmtexaux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # aux tex files to delete
-files="*.aux *.bbl *.blg *.bst *.dvi *.idx *.lof *.log *.toc *.bak *.out *.lot *.brf *.blx.bib *.synctex.gz *.cut *.loa *.lol *.tdo *.snm *.nav *.vrb"
+files="*.aux *.bbl *.blg *.bst *.dvi *.idx *.lof *.log *.toc *.bak *.out *.lot *.brf *.blx.bib *.synctex.gz *.cut *.loa *.lol *.tdo *.snm *.nav *.vrb *.bcf"
 for f in $files; do
   if [ -f $f ];	then
     rm $f
